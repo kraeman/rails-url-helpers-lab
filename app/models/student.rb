04e5f2054a1960a85757e84f4bf244360a11d5fb
krae <kraewind@gmail.com>
2021-03-02 13:53:28 -0500

    commit tree e09f3592e30038abbaeaa2a34e03fd49d23ee02c

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,22 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_message 
+    if self.active
+      "This student is currently active."
+    else
+      "This student is currently inactive."
+    end
+  end
+
+  def change_status
+    
+    if self.active
+      self.update(active: false)
+    else
+      self.update(active: true)
+    end
+  end
+
 end
